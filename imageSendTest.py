a159032ae1371da8bac0f6e1bc25c3e93f61afc7
tapi-y <tapi09ucat@gmail.com>
2018-06-21 10:41:56 +0900

    server send

--- imageSendTest.py
+++ imageSendTest.py
@@ -4,15 +4,18 @@ import time
 import scp
 import paramiko
 
+k = paramiko.RSAKey.from_private_key_file("/home/pi/rsa")
 
 def main():
     while(True):
-        time.sleep(5)
         with paramiko.SSHClient() as ssh:
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-            ssh.connect(hostname='10.144.116.253', port=22, username='pi', password='raspberry')
+            #ssh.connect(hostname='10.144.116.253', port=22, username='pi', password='raspberry')
+            ssh.connect(hostname = "ec2-13-56-144-143.us-west-1.compute.amazonaws.com", username = "ubuntu", pkey = k)
             with scp.SCPClient(ssh.get_transport()) as scptest:
-                scptest.put('alpha.png', '/home/pi/alpha.png')
+                print 'ok'
+                scptest.put('alpha.png', '~/work/panelx-prototype/panel/src/assets/img/washingmachine-hack/camera/alpha.png')
+        time.sleep(5)
 
 
 if __name__ == '__main__':
